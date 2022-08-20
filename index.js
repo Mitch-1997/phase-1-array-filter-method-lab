bafbb04099e2fd6273ecba38f1d2b42efd080d82
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-08-20 18:30:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, names){
+    let found = drivers.filter((drivers) =>drivers.toLowerCase() === names.toLowerCase());
+    return found;
+}    
+function fuzzyMatch(drivers, query){
+    let fuzzy = drivers.filter((drivers) => drivers.startsWith(query));
+    return fuzzy;
+}
+function matchName(drivers, string){
+let match = drivers.filter((drivers) => drivers.name=== string);
+return match;
+}
